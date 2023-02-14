8441d46e4a57116ce6b209874ead720fcfd014a6
buvaneshtmb <buvaneshtmb@gmail.com>
2023-02-14 15:43:11 +0530

    middleware

--- Schema/UserSchema.js
+++ Schema/UserSchema.js
@@ -17,7 +17,11 @@ const userSchema=new mongoose.Schema({
     password:{
         type:String,
         required:true,
-    },
+    },  
+    role:{
+        type:String,
+        default:"student"
+      },
     createdAt:{type:Date,default:Date.now()}
 
 },
--- common/auth.js
+++ common/auth.js
@@ -1,26 +1,66 @@
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken')
-const SALT=10;
-const secret='ndnlksad231@&^%^%jkNJWHSB'
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const SALT = 10;
+const secret = 'ndnlksad231@&^%^%jkNJWHSB'
 
-const hashPassword=async(password)=>{
-    let salt=await bcrypt.genSalt(SALT)
-    let hash=await bcrypt.hash(password,salt)
+const hashPassword = async (password) => {
+    let salt = await bcrypt.genSalt(SALT)
+    let hash = await bcrypt.hash(password, salt)
     return hash
 }
 
-const hashCompare=async(password,hashedPassword)=>{
-    return bcrypt.compare(password,hashedPassword)
+const hashCompare = async (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword)
 }
 
-const createToken=async(payload)=>{
-    let token = await jwt.sign(payload,secret,{expiresIn:'1d'})
+const createToken = async (payload) => {
+    let token = await jwt.sign(payload, secret, { expiresIn: '1m' })
     return token
 }
 
-const decodeToken=async(token)=>{
-    let data =await jwt.decode(token)
-    console.log(data)
+const decodeToken = async (token) => {
+    let data = await jwt.decode(token)
+    return data
 }
 
-module.exports={hashPassword,hashCompare,createToken,decodeToken}
+const validate = async (req, res, next) => {
+    if (req.headers.authorization) {
+        let token = req.headers.authorization.split(" ")[1]
+        let data = await decodeToken(token)
+        if (Math.round(Date.now() / 1000) <= data.exp) {
+            next()
+        }
+        else {
+            res.status(401).send({
+                message: "Token Expired"
+            })
+        }
+    }
+    else {
+        res.status(400).send({
+            message: "No Token Found"
+        })
+    }
+}
+
+const roleAdmin = async (req, res, next) => {
+    if (req.headers.authorization) {
+        let token = req.headers.authorization.split(" ")[1]
+        let data = await decodeToken(token)
+        if (data.role === 'admin') {
+            next()
+        }
+        else {
+            res.status(401).send({
+                message: "Only Admin Can Access"
+            })
+        }
+    }
+    else {
+        res.status(400).send({
+            message: "No Token Found"
+        })
+    }
+}
+
+module.exports = { hashPassword, hashCompare, createToken, decodeToken, validate, roleAdmin }
